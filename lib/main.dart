ad2b8c34e388b061534f5e9d9e7f1a24f6537062
hapham hapham <hapham@bbossystem.com>
2024-02-19 18:02:13 +0700

    add image picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,35 +10,33 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Image Picker Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ImagePickerScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class ImagePickerScreen extends StatefulWidget {
+  const ImagePickerScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  // ignore: library_private_types_in_public_api
+  _ImagePickerScreenState createState() => _ImagePickerScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ImagePickerScreenState extends State<ImagePickerScreen> {
+  XFile? _imageFile;
 
-  void _incrementCounter() {
+  Future<void> _pickImage(ImageSource source) async {
+    XFile? pickedImage = await ImagePicker().pickImage(source: source);
     setState(() {
-      _counter++;
+      _imageFile = pickedImage;
     });
   }
 
@@ -43,28 +44,29 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Image Picker Demo'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: _imageFile == null
+            ? const Text('No image selected')
+            : Image.file(File(_imageFile!.path)),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: () => _pickImage(ImageSource.camera),
+            tooltip: 'Pick Image from Camera',
+            child: const Icon(Icons.camera),
+          ),
+          const SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: () => _pickImage(ImageSource.gallery),
+            tooltip: 'Pick Image from Gallery',
+            child: const Icon(Icons.image),
+          ),
+        ],
+      ),
     );
   }
 }
